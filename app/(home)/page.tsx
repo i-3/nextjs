d952afa1e5352833c0472be362adb6934ff26da9
i-3 <iurii.korotkoff@gmail.com>
2025-06-23 21:18:04 +0300

    IP address

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -6,22 +6,15 @@ import Projects from './Projects';
 import Contact from './Contact';
 import Footer from './Footer';
 
-// import { NextRequest } from 'next/server';
 import { headers } from 'next/headers';
 
 export default async function Home() {
   const headersList = await headers();
-  // const xRealIp = request.headers.get('x-real-ip');
-  // const xForwardedFor = request.headers.get('x-forwarded-for');
-
-  // Use the IP addresses as needed
-  // let clientIp = xRealIp || xForwardedFor?.split(',')[0] || request.ip;
-  console.log(headersList.get('x-real-ip'));
-  console.log(headersList.get('x-forwarded-for'));
+  const ip = headersList.get('x-real-ip');
 
   return (
     <main className='flex flex-col min-h-screen'>
-      <p>{headersList}</p>
+      <p className=' text-center text-blue-500'>Your IP address: {ip}</p>
 
       <Navbar />
       <Hero />
