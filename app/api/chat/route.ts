f2be21682f16f8e1440d6616fe286ee690370e55
i-3 <iurii.korotkoff@gmail.com>
2025-11-28 17:31:29 +0200

    chat commented

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,6 +1,6 @@
 // import { openai } from '@ai-sdk/openai';
-import { groq } from "@ai-sdk/groq";
-import { streamText } from "ai";
+// import { groq } from "@ai-sdk/groq";
+// import { streamText } from "ai";
 
 // Allow streaming responses up to 30 seconds
 export const maxDuration = 30;
@@ -8,40 +8,40 @@ export const maxDuration = 30;
 export async function POST(req: Request) {
   const { messages } = await req.json();
 
-  const result = streamText({
-    // model: openai('gpt-4o'),
-    model: groq("gemma2-9b-it"),
-    messages,
-    // tools: {
-    //   weather: tool({
-    //     description: 'Get the weather in a location (fahrenheit)',
-    //     parameters: z.object({
-    //       location: z.string().describe('The location to get the weather for'),
-    //     }),
-    //     execute: async ({ location }) => {
-    //       const temperature = Math.round(Math.random() * (90 - 32) + 32);
-    //       return {
-    //         location,
-    //         temperature,
-    //       };
-    //     },
-    //   }),
-    //   convertFahrenheitToCelsius: tool({
-    //     description: 'Convert a temperature in fahrenheit to celsius',
-    //     parameters: z.object({
-    //       temperature: z
-    //         .number()
-    //         .describe('The temperature in fahrenheit to convert'),
-    //     }),
-    //     execute: async ({ temperature }) => {
-    //       const celsius = Math.round((temperature - 32) * (5 / 9));
-    //       return {
-    //         celsius,
-    //       };
-    //     },
-    //   }),
-    // },
-  });
+  // const result = streamText({
+  // model: openai('gpt-4o'),
+  // model: groq("gemma2-9b-it"),
+  // messages,
+  // tools: {
+  //   weather: tool({
+  //     description: 'Get the weather in a location (fahrenheit)',
+  //     parameters: z.object({
+  //       location: z.string().describe('The location to get the weather for'),
+  //     }),
+  //     execute: async ({ location }) => {
+  //       const temperature = Math.round(Math.random() * (90 - 32) + 32);
+  //       return {
+  //         location,
+  //         temperature,
+  //       };
+  //     },
+  //   }),
+  //   convertFahrenheitToCelsius: tool({
+  //     description: 'Convert a temperature in fahrenheit to celsius',
+  //     parameters: z.object({
+  //       temperature: z
+  //         .number()
+  //         .describe('The temperature in fahrenheit to convert'),
+  //     }),
+  //     execute: async ({ temperature }) => {
+  //       const celsius = Math.round((temperature - 32) * (5 / 9));
+  //       return {
+  //         celsius,
+  //       };
+  //     },
+  //   }),
+  // },
+  // });
 
   // return result.toDataStreamResponse();
 }
