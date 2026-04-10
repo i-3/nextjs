0f00e97ca5cc931a683ebeaa1b6badecc745137c
i-3 <31x1962@gmail.com>
2026-04-10 20:17:00 +0300

    ?

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,8 @@
-import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DATABASE_URL, // Оставляем только этот вариант
+    url: process.env.DATABASE_URL,
   },
 });
