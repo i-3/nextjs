c88b9473f85d78694c47d4260388f244fc77cff0
i-3 <31x1962@gmail.com>
2026-04-10 17:43:18 +0300

    resend_key

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,15 +1,15 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { Resend } from 'resend';
+import { NextRequest, NextResponse } from "next/server";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "re_123456789");
 
 export async function POST(req: NextRequest) {
   const { email, subject, message } = await req.json();
 
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['iurii.korotkoff@gmail.com'],
+      from: "Acme <onboarding@resend.dev>",
+      to: ["iurii.korotkoff@gmail.com"],
       subject: `${subject} (from ${email})`,
       // react: EmailTemplate({ firstName: 'John' }),
       text: message,
@@ -23,8 +23,8 @@ export async function POST(req: NextRequest) {
     //data = { id: 'd88acf57-172d-44a4-b987-65f9e879c1de' }
 
     return Response.json(
-      { message: 'Email sent successfully', data },
-      { status: 200 }
+      { message: "Email sent successfully", data },
+      { status: 200 },
     );
   } catch (error) {
     return Response.json({ error }, { status: 500 });
